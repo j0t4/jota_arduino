560fd2ec2e0b7f952718ad9240da08c2f9c1434e
j0t4 <j0t4@j0t4.com>
2017-07-15 19:36:58 +0200

    upload first commit.

---
+++ GPS_milis_simple/GPS_milis_simple.ino
@@ -0,0 +1,36 @@
+//#/////////  JOTA: mira los niveles en el pin 2 y ajus te del generador de señal.
+//#void setup(){
+//#  pinMode(3, INPUT_PULLUP);
+//#  pinMode(13, OUTPUT);
+//#  Serial.begin(115200);   
+//#}
+//#
+//#void loop(){
+//#while(digitalRead(3)==1){
+//#  Serial.println("1");
+//#  digitalWrite(13, HIGH);
+//#}
+
+//#while(digitalRead(3)==0){
+//#  Serial.println(".");
+//#  digitalWrite(13, LOW);
+//#}
+//#delay(50);
+//#}
+
+long ms=0;
+
+void setup(){
+   Serial.begin(115200);
+    pinMode(3, INPUT); 
+    delay(2000);
+}
+void loop(){
+    while(digitalRead(3)==0);
+    while(digitalRead(3)==1);
+    long ms= millis();
+    while(digitalRead(3)==0); 
+    long m=millis();
+    long quinientos=m-ms; 
+    Serial.println(quinientos);
+}
---
+++ PPS_counter_test_2.ino/PPS_counter_test_2.ino.ino
@@ -0,0 +1,57 @@
+volatile unsigned long pps_cuenta = 0;
+volatile unsigned long multiplicador = 0;
+volatile unsigned long frecuencia = 0;
+
+void gpsPPS()
+{
+  pps_cuenta++;
+  if (pps_cuenta == 1)                           // Start counting the 2.5 MHz signal from Si5351A CLK0
+  {
+    TCCR1B = 7;                                  //Clock on rising edge of pin 5
+  }
+  else if (pps_cuenta == 65)                     //The 40 second gate time elapsed - stop counting
+  {     
+    frecuencia = TCNT1 + ( multiplicador << 16);//Calculate correction factor
+    TCCR1A = 0;                                  //Turn off counter
+    TCCR1B = 0; 
+    TCNT1 = 0;                                   //Reset count to zero
+    TIFR1  = 1;                                    //Reset overflow
+    TIMSK1 = 1;                                    //Turn on overflow flag
+    multiplicador = 0;
+    pps_cuenta = 0;                                  //Reset the seconds counter
+  }
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  multiplicador++;
+  //TIFR1 = (1<<TOV1); 
+}
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+  Serial.println("\nHello.\n");
+  
+  //Set up Timer1 as a frequency counter - input at pin 5
+  TCCR1B = 0;                                    //Disable Timer5 during setup
+  TCCR1A = 0;                                    //Reset
+  TCNT1  = 0;                                    //Reset counter to zero
+  TIFR1  = 1;                                    //Reset overflow
+  TIMSK1 = 1;                                    //Turn on overflow flag
+  pinMode(5, INPUT);  // Este es el pin para contar el Timer1 de 16 bits
+  pinMode(2, INPUT);  // Este es el pin para interrupir con el pps en INT0
+  attachInterrupt(INT0, gpsPPS, RISING);
+
+}
+
+void loop()                     // run over and over again
+{
+    delay(60000);
+    Serial.print(multiplicador);
+    Serial.print("  -->  ");
+    unsigned long frecuencia2 = (frecuencia >> 6);
+    Serial.println(frecuencia2);
+
+}
